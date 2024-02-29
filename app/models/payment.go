aa7e53906496b467ecad966f38b1dacad7fb792f
JoelD7 <joeld200013@gmail.com>
2024-02-29 14:41:39 -0400

    Create PaymentStatus type

--- app/models/payment.go
+++ app/models/payment.go
@@ -4,18 +4,26 @@ import (
 	"time"
 )
 
+type PaymentStatus string
+
+const (
+	PaymentStatusProcessing PaymentStatus = "processing"
+	PaymentStatusFailed     PaymentStatus = "failed"
+	PaymentStatusSuccess    PaymentStatus = "success"
+)
+
 type Payment struct {
-	ID            *string    `json:"id" gorm:"id"`
-	MerchantID    *string    `json:"merchantID" gorm:"merchant_id"`
-	CustomerID    *string    `json:"customerID" gorm:"customer_id"`
-	CardNumber    *int64     `json:"cardNumber" gorm:"card_number"`
-	TransactionID *string    `json:"transactionID" gorm:"transaction_id"`
-	Amount        *float64   `json:"amount" gorm:"amount"`
-	Status        *string    `json:"status" gorm:"status"`
-	FailureReason *string    `json:"failureReason" gorm:"failure_reason"`
-	CreatedDate   *time.Time `json:"createdDate" gorm:"created_date"`
-	UpdatedDate   *time.Time `json:"updatedDate" gorm:"updated_date"`
-	Customer      Customer   `json:"-" gorm:"foreignKey:CustomerID"`
-	Merchant      Merchant   `json:"-" gorm:"foreignKey:MerchantID"`
-	Card          Card       `json:"-" gorm:"foreignKey:CardNumber"`
+	ID            *string        `json:"id" gorm:"id"`
+	MerchantID    *string        `json:"merchantID" gorm:"merchant_id"`
+	CustomerID    *string        `json:"customerID" gorm:"customer_id"`
+	CardNumber    *int64         `json:"cardNumber" gorm:"card_number"`
+	TransactionID *string        `json:"transactionID" gorm:"transaction_id"`
+	Amount        *float64       `json:"amount" gorm:"amount"`
+	Status        *PaymentStatus `json:"status" gorm:"status"`
+	FailureReason *string        `json:"failureReason" gorm:"failure_reason"`
+	CreatedDate   *time.Time     `json:"createdDate" gorm:"created_date"`
+	UpdatedDate   *time.Time     `json:"updatedDate" gorm:"updated_date"`
+	Customer      Customer       `json:"-" gorm:"foreignKey:CustomerID"`
+	Merchant      Merchant       `json:"-" gorm:"foreignKey:MerchantID"`
+	Card          Card           `json:"-" gorm:"foreignKey:CardNumber"`
 }
