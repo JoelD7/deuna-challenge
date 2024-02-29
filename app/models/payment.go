24f0f251b99fae3f22dbc72e75c263046190473a
JoelD7 <joeld200013@gmail.com>
2024-02-29 14:38:59 -0400

    Make field type pointers
    Why: To detect nil/absent values

--- app/models/payment.go
+++ app/models/payment.go
@@ -5,14 +5,14 @@ import (
 )
 
 type Payment struct {
-	ID            string     `json:"id" gorm:"id"`
-	MerchantID    string     `json:"merchantID" gorm:"merchant_id"`
-	CustomerID    string     `json:"customerID" gorm:"customer_id"`
-	CardNumber    int64      `json:"cardNumber" gorm:"card_number"`
-	TransactionID string     `json:"transactionID" gorm:"transaction_id"`
-	Amount        float64    `json:"amount" gorm:"amount"`
-	Status        string     `json:"status" gorm:"status"`
-	FailureReason string     `json:"failureReason" gorm:"failure_reason"`
+	ID            *string    `json:"id" gorm:"id"`
+	MerchantID    *string    `json:"merchantID" gorm:"merchant_id"`
+	CustomerID    *string    `json:"customerID" gorm:"customer_id"`
+	CardNumber    *int64     `json:"cardNumber" gorm:"card_number"`
+	TransactionID *string    `json:"transactionID" gorm:"transaction_id"`
+	Amount        *float64   `json:"amount" gorm:"amount"`
+	Status        *string    `json:"status" gorm:"status"`
+	FailureReason *string    `json:"failureReason" gorm:"failure_reason"`
 	CreatedDate   *time.Time `json:"createdDate" gorm:"created_date"`
 	UpdatedDate   *time.Time `json:"updatedDate" gorm:"updated_date"`
 	Customer      Customer   `json:"-" gorm:"foreignKey:CustomerID"`
